00d9d96d2ff2b95908ebec1869f5ea0f5161c960
Shubham <shubham@esamudaay.com>
2021-06-03 11:05:58 +0530

    Added Staging URL

--- src/Utility/config.js
+++ src/Utility/config.js
@@ -1,4 +1,15 @@
-const apiUrl = 'http://127.0.0.1:8000/api/v1/orders/';
+let IS_STAGING = true;
+let IS_LOCAL = false;
+let IS_PROD = false;
+
+if (IS_STAGING) {
+    const apiUrl = 'https://api.test.esamudaay.com/api/v1/orders/';
+}
+else if (IS_PROD) {
+    const apiUrl = 'https://api.prod.esamudaay.com/api/v1/orders/';
+}
+
+
 
 const config = {
     apiUrl,
@@ -8,4 +19,3 @@ export default config;
 
 
 
-//http://localhost:3001/dd2f1bd7-d8e7-48b6-8a0a-73ec40731146/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoyLCJpc19hY3RpdmUiOnRydWUsImlzX3N1c3BlbmRlZCI6ZmFsc2UsInBob25lIjoiKzkxOTMwMjgxMTU5NyIsInRoaXJkX3BhcnR5X2lkIjoiZmQwZWFlNGEtODBhMC00ODkzLTkyMDMtMDJjY2RlNDc0MzI3IiwicHJvZmlsZSI6eyJyb2xlIjoiUFJPVklERVIiLCJzdXNwZW5kZWQiOmZhbHNlfSwiZXhwIjoxNjI4NDEwODgzLCJvcmlnX2lhdCI6MTYyMDYzNDg4M30.uRwg2AUmzSIoOU0kWWarfHUcyrUNLGjk1yMh15Laj5c
